998f1acdf805b5639e7603276d92c371e3d28c5f
adhmkt <152990230+adhmkt@users.noreply.github.com>
2024-01-10 12:53:26 -0300

    Production Config

--- config_production.py
+++ config_production.py
@@ -1,5 +1,5 @@
 class ProductionConfig:
-    DEBUG = False
-    REDIS_TLS_URL =  "rediss://:pb832bd232f647cd9d6f05ebf87d850310ef9d4c76c6bcdadebd0b91838b0987d@ec2-44-199-142-192.compute-1.amazonaws.com:6380"
-    REDIS_URL = "redis://:pb832bd232f647cd9d6f05ebf87d850310ef9d4c76c6bcdadebd0b91838b0987d@ec2-44-199-142-192.compute-1.amazonaws.com:6379"
-    # ... other production-specific settings ...
+    import os
+    EBUG = False
+    REDIS_TLS_URL = os.environ.get('REDIS_TLS_URL')
+    REDIS_URL = os.environ.get('REDIS_URL')
